efb1807afc743bf201f77eec15fc49a24c5047d0
ananthvishnu <vishnurathan834@gmail.com>
2023-02-10 22:19:14 +0530

    first add assihnment in git

---
+++ Home.js
@@ -0,0 +1,4 @@
+//one click function in navbar button
+//let btnPrimary = document.querySelector('#primary');
+
+//btnPrimary.addEventListener('click', () => btnPrimary.style.backgroundColor='green');
